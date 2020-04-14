9f57f499451853bf3e124f2062aec9aa28d35684
hmedina3 <hmedina3@ucsc.edu>
2020-04-14 04:06:47 -0700

    Final Commit

--- src/main.js
+++ src/main.js
@@ -4,4 +4,13 @@ let config = {
     height: 480,
     scene: [Menu, Play]
   }
+  
+  // reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
+
 let game = new Phaser.Game(config); 
+// define game settings
+game.settings = {
+  spaceshipSpeed: 3,
+  gameTimer: 60000    
+}
